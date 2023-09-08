c7dfbfe4a9de0ef22e05aa21462c0eb5c3369d98
Angel de la Torre <angel.dlt.1996@gmail.com>
2023-09-08 13:43:00 -0700

    feat: add DatadogClient class
    This class will be a wrapper around the datadog-api-client package.
    For the moment, it will only have a method to send metrics to Datadog.
    That said, we will require some api keys to be able to send metrics, so
    let's create an .env file and import it via dotenv.

---
+++ src/DatadogClient.ts
@@ -0,0 +1,40 @@
+import { client, v2 } from '@datadog/datadog-api-client';
+
+
+export default class DatadogClient {
+    private clientConfiguration: client.Configuration;
+
+    constructor({ api_key, app_key}: { api_key: string, app_key: string }) {
+        const configurationOpts = {
+            authMethods: {
+                apiKeyAuth: api_key,
+                appKeyAuth: app_key
+            },
+        };
+
+        this.clientConfiguration = client.createConfiguration(configurationOpts);
+    }
+
+    async submitMetrics(metricName: string, dataPoints: v2.MetricPoint[], tags: string[] = []): Promise<void> {
+        const metricsApiInstance = new v2.MetricsApi(this.clientConfiguration);
+
+        const params: v2.MetricsApiSubmitMetricsRequest = {
+            body: {
+                series: [
+                    {
+                        metric: metricName,
+                        type: 3, // gauge
+                        points: dataPoints,
+                        tags: tags
+                    }
+                ]
+            }
+        };
+
+        const response = await metricsApiInstance.submitMetrics(params);
+
+        if (response.errors && response.errors.length > 0) {
+            throw new Error(response.errors.join(', '));
+        }
+    }
+}
