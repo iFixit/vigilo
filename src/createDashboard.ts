75e930b653e7e60afb068c52f19528b8a6d2c367
batbattur <bat@ifixit.com>
2023-10-04 09:32:01 -0700

    createDashboard: add function to prepare the api body
    The values in `templateVariables` were copy/pasted from a get request
    to match the existing Lighthouse reports dashboard.
    
    In the next commit, we will be updating the widgets in the body.

--- src/createDashboard.ts
+++ src/createDashboard.ts
@@ -9,12 +9,30 @@ const inspectList = JSON.parse(fs.readFileSync('urls.json', 'utf8'));
 const coreMetrics = JSON.parse(fs.readFileSync('metrics-config.json', 'utf8'));
 const host = 'ubreakit.com';
 
+function getDashboardsApiBody(): v1.DashboardsApiCreateDashboardRequest {
+    return {
+        body: {
+            title: "Temporary Lighthouse Reports",
+            templateVariables: [
+                {
+                  name: "FormFactor",
+                  prefix: "form_factor",
+                  availableValues: [],
+                  _default: "*",
+                },
+            ],
+            layoutType: "ordered",
+            widgets: [],
+        }
+    }
+}
+
 (async() => {
     const dd = new Datadog({
         api_key: process.env.DD_API_KEY || '',
         app_key: process.env.DD_APP_KEY || ''
     })
 
-    const params: v1.DashboardsApiCreateDashboardRequest = {};
+    const params: v1.DashboardsApiCreateDashboardRequest = getDashboardsApiBody();
     await dd.createDashboard(params)
 })()
