82a96dead43d6e80782d1042f56604c4fb7d29f9
Angel de la Torre <angel.dlt.1996@gmail.com>
2023-10-12 10:11:35 -0700

    feat: set yargs options for command
    We will only allow updating a subset of the metadata fields, the other
    fields are not crucial for representing Lighthouse metrics.

--- src/updateDatadogMetricMetadata.ts
+++ src/updateDatadogMetricMetadata.ts
@@ -1,1 +1,38 @@
 import yargs from 'yargs'
+
+const argv = yargs(process.argv.slice(2)).options(
+    {
+        auditName: {
+            type: 'string',
+            demandOption: true,
+            description: 'Lighthouse audit name to update.',
+        },
+        auditType: {
+            type: 'string',
+            choices: ['score', 'value'],
+            demandOption: true,
+            description: 'Lighthouse audit type to update. Either "score" or "value"',
+        },
+        unit: {
+            type: 'string',
+            description: 'Datadog metric unit type for graph display.',
+        },
+        type: {
+            type: 'string',
+            choices: ['gauge', 'count', 'rate'],
+            description: 'Datadog metric type',
+        },
+        description: {
+            type:'string',
+            description: 'Datadog metric description.'
+        },
+    })
+    .parseSync();
+
+const auditName =  argv.auditName
+const auditType = argv.auditType as 'score' | 'value'
+const metaData = {
+    unit: argv.unit,
+    type: argv.type as 'gauge' | 'count' | 'rate',
+    description: argv.description
+};
